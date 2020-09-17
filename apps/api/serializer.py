3fd49ffd0ad421b9dd8b0beb322cfdc9c74c3c02
weilyl <weily.lang@gmail.com>
2020-09-17 12:58:41 -0400

    untested views

--- apps/api/serializer.py
+++ apps/api/serializer.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
-from apps.api.models import Link, List
+from apps.api.models import Link, List, Tag, Review
 
 
 # referenced relationship first
+class ReviewSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Review
+        fields = ('id', 'title', 'notes', 'difficulty')
+
+
+class TagSerializer(serializers.ModelSerializer):
+    link = serializers.ReadOnlyField(source='link.name')
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+
+
 class LinkSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Link
-        fields = ('id', 'list', 'name', 'description', 'created_at', 'updated_at',
-                  'is_public', 'is_favorite', 'is_saved')
+        fields = ('id', 'list', 'name', 'description', 'image', 'created_at', 'updated_at',
+                  'is_public', 'is_favorite', 'is_saved', 'tags')
 
 
 class ListSerializer(serializers.ModelSerializer):
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,93 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics  # contains filters so spec categories belong to spec users
+from rest_framework import viewsets
+from rest_framework.exceptions import (
+    ValidationError, PermissionDenied
+)
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from apps.api.models import Link, List, Tag, Review
+from apps.api.serializer import LinkSerializer, ListSerializer, TagSerializer, ReviewSerializer
 
-# Create your views here.
+
+class ListViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ListSerializer
+
+    def get_queryset(self):
+        queryset = List.objects.all().filter(
+            owner=self.request.user
+        )
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        user_list = List.objects.filter(
+            name=request.data.get('name'),
+            owner=request.user,
+        )
+        if user_list:
+            msg = 'You already have a list with that name. Do you want to add to that list instead?'
+            raise ValidationError(msg)
+        return super().create(request)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def perform_destroy(self, instance):
+        pass
+
+    def destroy(self, request, *args, **kwargs):
+        user_list = List.objects.get(pk=self.kwargs['pk'])
+        if not request.user == user_list.owner:
+            raise PermissionDenied("You don't have permission to delete this list")
+        super().destroy(request, *args, **kwargs)
+        return Response(
+            {
+                'message': f'{user_list} has been deleted',
+                'status': status.HTTP_200_OK
+            }
+        )
+
+
+class ListLinks(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = LinkSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('list_pk'):
+            user_list = List.objects.get(pk=self.kwargs['list_pk'])
+            queryset = Link.objects.filter(
+                owner=self.request.user,
+                list=user_list,
+            )
+            return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+    class SingleLinkPerList(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = LinkSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('list_pk') and self.kwargs.get('pk'):
+            user_list = List.objects.get(pk=self.kwargs['list_pk'])
+            queryset = Link.objects.filter(
+                pk=self.kwargs['pk'],
+                owner=self.request.user,
+                list=user_list
+            )
+            return queryset
+
+
+class LinksViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = LinkSerializer
+
+    def get_queryset(self):
+        queryset = Link.objects.all().filter(
+            user_list=List.objects.filter(owner=self.request.user)
+        )
+        return queryset
